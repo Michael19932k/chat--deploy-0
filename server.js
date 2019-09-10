62b9c45edc504800305ef21581f479b00ad996fd
Michael <Michael>
2019-09-10 20:16:26 +0300

    FIX 26

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const _ = require('lodash');
-const port = process.env.PORT || 3001;
+app.set('port', (process.env.PORT || 5000));
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-var http = require('http').createServer(app);
-var io = require('socket.io').listen(http);
+var server = app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+var io = require('socket.io')(server);
 app.use(express.static('public'))
 app.use(cors());
 app.use(bodyParser.json());
